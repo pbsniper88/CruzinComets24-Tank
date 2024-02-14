b6dabc953be3e4f4e9ddabb3385cfeab7e368797
Seamus Allison <121757196+TankTheTortoise@users.noreply.github.com>
2024-02-14 15:29:29 -0500

    Update Constants.java Wheel diameter

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -69,7 +69,7 @@ public final class Constants {
   public static final double AutoStallRate = 0.0;
   public static final double AutoDriveRate = 0.5;
   public static final double AutoReverseRate = -0.5;
-  public static final double WheelDiameter = ; // Feet
+  public static final double WheelDiameter = 0.5; // Feet
   public static final double Pi = 3.14159265358979; // 15 digits
 
 }
