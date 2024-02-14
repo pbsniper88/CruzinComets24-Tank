3d8e7e8f03c1e6000ae257693802fa4c33fc9375
Seamus Allison <121757196+TankTheTortoise@users.noreply.github.com>
2024-02-14 12:20:18 -0500

    Smooth Autonomous Drive (#6)
    * sdf
    
    * Smooth Drive And Constants

---
+++ src/main/java/frc/robot/commands/Autonomous/smoothDrive.java
@@ -0,0 +1,59 @@
+package frc.robot.commands.Autonomous;
+import frc.robot.subsystems.TankDrive;
+import frc.robot.Constants;
+import edu.wpi.first.wpilibj.Encoder;
+
+public class SmoothDriveForwardAction implements Action {
+    //Determines how long to drive forward
+    private double distance;
+    //Keeps track of when this action started
+    private long startTime;
+    //Variable that accesses our TankDrive subsystem
+    private TankDrive tankDrive;
+    Encoder encoder = new Encoder(0, 1);
+    // Configures the encoder's distance-per-pulse
+    // The robot moves forward 1 foot per encoder rotation
+    // There are 256 pulses per encoder rotation
+    encoder.setDistancePerPulse(Constants.WheelDiameter*Constants.Pi/256.0);
+    encoder.setSamplesToAverage(5);
+
+    
+    //Constructer is called within Robot class, which contains the an instance of TankDrive
+    // Distance is in feet
+    public DriveForwardAction(double distance, TankDrive tankDrive) {
+        this.distance = distance;
+        this.tankDrive = tankDrive;
+        if(encoder.getDistance() < distance) {
+            tankDrive.autonDrive(Constants.AutoDriveRate, Constants.AutoDriveRate);
+        } 
+        else {
+            tankDrive.autonDrive(Constants.AutoStallRate, Constants.AutoStallRate);
+        }
+    }
+
+    @Override
+    public void start() {
+        startTime = System.currentTimeMillis();
+        // Code to start driving forward
+        //Speed for autonDrive is within Constants class
+        tankDrive.autonDrive(Constants.AutoDriveRate, Constants.AutoDriveRate);
+    }
+
+    @Override
+    public void update() {
+        // Code to update driving logic (if necessary)
+    }
+
+    @Override
+    public void end() {
+        // Code to stop the robot
+        tankDrive.autonDrive(Constants.AutoStallRate, Constants.AutoStallRate);
+    }
+
+    @Override
+    public boolean isFinished() {
+        //This determines how long the Action runs based on how long ago it started
+        //end() will be called once this returns true        
+        return (System.currentTimeMillis() - startTime) >= duration * 1000;
+    }
+}
